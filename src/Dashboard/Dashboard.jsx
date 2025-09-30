1419e401f216f124e11a9fef44ee575d9540be62
Paban-1 <rpaban89@gmail.com>
2025-09-30 21:58:52 +0530

    Update dashboard UI

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,23 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { BiMenu, BiX } from "react-icons/bi";
-import { logo } from "../constents";
+import {MdSettingsInputHdmi} from '../constents'
+import { InzoIcon } from "../constents";
+import { HoverEffect } from "../constents";
+import { rotate } from "../constents";
 
 const Dashboard = () => {
   const [open, setOpen] = useState(false);
+  const sideRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (sideRef.current && !sideRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    }
+
+    if(open){
+      document.addEventListener("mousedown", handleClickOutside)
+    }else{
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+
+    return ()=>{
+      document.removeEventListener("mousedown",handleClickOutside)
+    }
+  },[open]);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-[170px_1fr] h-screen">
       {/* SideBar  */}
       <aside
         className={`bg-white text-black shadow p-4 fixed md:static h-full w-44 transform ${
           open ? "translate-x-0" : "-translate-x-full"
-        } md:translate-x-0 transition-transform duration-300 z-50`}
+        } md:translate-x-0 transition-transform duration-300 z-50 `}
+        ref={sideRef}
       >
         <div className="flex justify-between items-center text-black mb-6">
-          <div className="text-xl font-bold">
-            <img src={logo} alt="" className="h-10 rounded-md scale-75" />
+          <div className="text-xl flex justify-center items-center font-bold">
+            <img
+              src={InzoIcon}
+              alt="Logo"
+              className="scale-105 h-10 rounded-md"
+            />
+            <label>Inz</label>
+            <label className={`${rotate.rota}`}>o</label>
           </div>
-          <button onClick={() => setOpen(!open)} className="md:hidden">
+          <button
+            onClick={() => setOpen(!open)}
+            className={`md:hidden border rounded-md bg-gray-300/30`}
+          >
             {open ? (
               <BiX className="w-6 h-6" />
             ) : (
@@ -26,22 +59,25 @@ const Dashboard = () => {
           </button>
         </div>
 
-        <nav className="flex flex-col gap-4 mb-8 border-t-2 border-gray-200">
-          <p>Home</p>
-          <p>Analytics</p>
-          <p>Settings</p>
+        <nav className="flex flex-col gap-2 mb-8 border-t-2 pt-1 border-gray-200 font-semibold">
+          <p className={`${HoverEffect.hover}`}>Home</p>
+          <p className={`${HoverEffect.hover}`}>Analytics</p>
+          <p className={`${HoverEffect.hover}`}>Settings</p>
         </nav>
 
-        <nav className="flex flex-col gap-4 border-t-2 border-gray-200">
-          <p>Home</p>
-          <p>Analytics</p>
-          <p>Settings</p>
+        <nav className="flex flex-col gap-2 border-t-2 pt-1 border-gray-200 font-semibold">
+          <p className={`${HoverEffect.hover}`}>About App</p>
+          <p className={`${HoverEffect.hover}`}>Analytics</p>
+          <p className={`${HoverEffect.hover}`}>Settings</p>
         </nav>
       </aside>
 
-      <main className="bg-gray-100 p-6 md:ml-0 ml-0">
+      <main className="bg-gray-100 p-6 md:ml-0 ml-0 ">
         <div className="md:hidden flex items-start justify-between mb-4">
-          <button onClick={() => setOpen(!open)} className="border ">
+          <button
+            onClick={() => setOpen(!open)}
+            className="border rounded-md bg-gray-300/30"
+          >
             {open ? (
               <BiX className="w-6 h-6" />
             ) : (
--- src/constents/index.js
+++ src/constents/index.js
@@ -1,6 +1,19 @@
 import Dashboard from "../Dashboard/Dashboard";
 
+import InzoIcon from "../assets/InzoIcon.png";
+import InzoLogo from "../assets/InzoLogo.jpg";
+import logo from "../assets/chila.jpeg";
 
-import logo from '../assets/chila.jpeg'
+export { Dashboard, InzoLogo, InzoIcon, logo };
 
-export {Dashboard, logo}
+
+// custom Css
+export const HoverEffect= {hover:"hover:bg-black hover:text-white duration-300 rounded-md px-1 cursor-pointer"}
+export const SimpleHover={hover:"hover:scale-105 duration-300"}
+export const rotate = {
+    rota:"inline-block transition-transform duration-300 hover:rotate-180"
+}
+
+// Import icons from react-icons
+import { MdSettingsInputHdmi } from "react-icons/md";
+export {MdSettingsInputHdmi}
