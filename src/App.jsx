f10980b19a8d28349fa75d43b852d1a651606d70
Paban-1 <rpaban89@gmail.com>
2025-10-12 14:21:25 +0530

    setup two

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-4xl text-red-500'>App</div>
+    <div className='text-4xl text-red-500 font-bold'>Test Text</div>
   )
 }
 
