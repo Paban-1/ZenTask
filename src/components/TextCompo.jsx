b4a59d4547dd6fd84ed8bf63811665e37501caa4
Paban-1 <rpaban89@gmail.com>
2025-10-14 20:01:41 +0530

    feat(ui): Add reusable base components (InputField, Button, TextCompo)

---
+++ src/components/TextCompo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TextCompo = () => {
+  return (
+    <div className="border absolute inset-0 z-0">
+      <div className="border flex flex-col gap-6 justify-center items-center h-full" style={{ fontFamily: "fantasy" }}>
+        <h2 className="text-[250px]" >
+          ZenTask
+        </h2>
+
+        <h2 className="text-[150px]">
+          ZenTask
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default TextCompo;
