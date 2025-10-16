d98e8ecd78d78b80a4b287c338dafdab9e034224
Paban-1 <rpaban89@gmail.com>
2025-10-16 22:38:22 +0530

    feat: export loginpage in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+// Import Pages & Components 
 import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
 import AnimatedTextBackground from "./components/AnimatedTextBackground";
 import InputField from "./components/InputField";
 import Button from "./components/Button";
 
-export { RegisterPage, AnimatedTextBackground, InputField, Button };
+// Export all Imports
+export { RegisterPage,LoginPage, AnimatedTextBackground, InputField, Button };
