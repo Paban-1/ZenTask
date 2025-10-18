e967c46e8b9a89abf29103ec44122f282ce2a7ec
Paban-1 <rpaban89@gmail.com>
2025-10-18 21:07:50 +0530

    FEAT: Implement base Dashboard UI and core components

---
+++ src/components/ContentSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContentSection = () => {
+  return (
+    <div className="h-[200vh] w-full"></div>
+  )
+}
+
+export default ContentSection
---
+++ src/components/TitleSection.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Logo,FiChevronDown } from "../constants/index.js"
+import {motion} from 'framer-motion'
+
+const TitleSection = ({ open }) => {
+  return (
+        <div className="mb-3 border-b border-slate-300 pb-3">
+          <div className="flex cursor-pointer items-center justify-between rounded-md transition-colors hover:bg-slate-100">
+            <div className="flex items-center gap-2">
+              <Logo />
+              {open && (
+                <motion.div
+                  layout
+                  initial={{ opacity: 0, y: 12 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: 0.125 }}
+                >
+                  <span className="block text-xs font-semibold">TomIsLoading</span>
+                  <span className="block text-xs text-slate-500">Pro Plan</span>
+                </motion.div>
+              )}
+            </div>
+            {open && <FiChevronDown className="mr-2" />}
+          </div>
+        </div>
+      );
+    };
+
+export default TitleSection
