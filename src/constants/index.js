f16a38188539320b1bf689711a3a1b4c6c840eff
Paban-1 <rpaban89@gmail.com>
2025-10-20 14:41:15 +0530

    Add: add new components & images and export

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,17 +7,17 @@ import HeroSection from "../components/HeroSection.jsx";
 import Features from "../components/Features.jsx";
 import Footer from "../components/Footer.jsx";
 import Dashboard from "../pages/Dashboard.jsx";
-import ContentSection from '../components/ContentSection.jsx'
-import Logo from '../components/Logo.jsx'
-import Option from '../components/Option.jsx'
-import SidebarSection from '../components/SidebarSection.jsx'
-import TitleSection from '../components/TitleSection.jsx'
-import ToggleClose from '../components/ToggleClose.jsx'
+import ContentSection from "../components/ContentSection.jsx";
+import Logo from "../components/Logo.jsx";
+import Option from "../components/Option.jsx";
+import SidebarSection from "../components/SidebarSection.jsx";
+import TitleSection from "../components/TitleSection.jsx";
+import ToggleClose from "../components/ToggleClose.jsx";
 
 import AnimatedTextBackground from "../components/AnimatedTextBackground.jsx";
 import InputField from "../components/InputField.jsx";
 import Button from "../components/Button.jsx";
-import ProtactedRoute from "../utils/ProtactedRoute.js"
+import ProtactedRoute from "../utils/ProtactedRoute.js";
 
 // Export all Imports
 export {
@@ -51,7 +51,6 @@ import {
 
 export { registerUser, loginUser, logOutUser, getCurrentUser };
 
-
 // Import Icons form react-Icons
 import { BiMenu } from "react-icons/bi";
 import {
@@ -65,7 +64,8 @@ import {
   FiTag,
   FiUsers,
 } from "react-icons/fi";
-export {BiMenu,
+export {
+  BiMenu,
   FiBarChart,
   FiChevronDown,
   FiChevronsRight,
@@ -75,4 +75,10 @@ export {BiMenu,
   FiShoppingCart,
   FiTag,
   FiUsers,
-}
+};
+
+// Import Images
+import DashPic from "../assets/zendashboard.png";
+import InzoIconZoomed from '../assets/InzoIconZoomed.png'
+// Export Images
+export { DashPic ,InzoIconZoomed};
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,9 +1,12 @@
 // Export Nav Items
 export const NAVITEMS = [
   { id: 1, label: "Home" },
-  { id: 2, label: "Product" },
-  { id: 3, label: "About" },
-  { id: 4, label: "Buy" },
+  { id: 2, label: "Features" },
+  { id: 3, label: "Pricing" },
 ];
 
-
+// Export More Option
+export const NAVITEMS2 = [
+  { id: 1, label: "Contact" },
+  { id: 2, label: "Pages" },
+];
